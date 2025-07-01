9e93720a419b2d35623afcad651f1efbe3239d4c
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-01 14:33:02 +0200

    upd prompt km + age

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const km = parseInt(prompt(`Inserisci la quatità di kilometri che vuoi percorrere`))
-console.log(km)
+//const km = parseInt(prompt(`Inserisci la quatità di kilometri che vuoi percorrere`))
+//console.log(km)
 
-const age = parseInt(prompt(`Inserisci la tua età`))
-console.log(age)
+//const age = parseInt(prompt(`Inserisci la tua età`))
+//console.log(age)
+
+//const randomeNumberKm = Math.floor(Math.random() * 100) + 1
+//console.log(randomeNumberKm)
+
+const randomeNumberKm = Math.floor(Math.random() * 100) + 1
+const userNumberKm = parseInt(prompt(`Inserisci il numero di kilometri che vuoi percorrere`))
+
+const randomeNumberAge = Math.floor(Math.random() * 75) + 1
+const userNumberAge = parseInt(prompt(`Inserisci la tua età`))
