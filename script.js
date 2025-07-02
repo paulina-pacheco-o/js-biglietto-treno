b4a94382980e158fdd5e17d0dc794a9c7b08b70f
paulina-pacheco-o <paulimichi05@gmail.com>
2025-07-02 16:13:23 +0200

    correzione lezione

--- script.js
+++ script.js
@@ -2,21 +2,25 @@ const km = parseInt(prompt(`Inserisci il numero di kilometri che vuoi percorrere
 
 const age = parseInt(prompt(`Inserisci la tua età`))
 
-let originalPrice = km * 0.21
+let finalPrice;
+const originalPrice = km * 0.21
+
 //prezzoScontato = prezzoIntero - prezzoIntero * valorePercentuale / 100
 //prezzoScontato = prezzoIntero * (1 - valorePercentuale / 100)
 //prezzi
+
+
 if (age < 18) {
-  originalPrice = originalPrice - originalPrice * 20 / 100
+  finalPrice = originalPrice - originalPrice * 20 / 100
 }
-console.log(originalPrice)
 
-if (age > 65) {
-  originalPrice = originalPrice - originalPrice * 40 / 100
+else if (age > 65) {
+  finalPrice = originalPrice - originalPrice * 40 / 100
 }
-console.log(originalPrice)
 
-//else {
-//originalPrice = originalPrice
-//}
 
+else {
+  finalPrice = originalPrice
+}
+
+console.log(`Il prezzo finale del biglietto è di ${finalPrice.toFixed(2)}`)
